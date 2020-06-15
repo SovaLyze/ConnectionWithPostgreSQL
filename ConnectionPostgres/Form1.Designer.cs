0db7f1f6cbfaf3f3b2ce35b79c402545de9ad200
Sova <yuralakey22@gmail.com>
2020-06-15 23:52:53 +0200

    WindowsForm connected with PostgreSQL

---
+++ ConnectionPostgres/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace ConnectionPostgres
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtMidName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvData
+            // 
+            this.dgvData.AllowUserToAddRows = false;
+            this.dgvData.AllowUserToDeleteRows = false;
+            this.dgvData.AllowUserToOrderColumns = true;
+            this.dgvData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(12, 131);
+            this.dgvData.MultiSelect = false;
+            this.dgvData.Name = "dgvData";
+            this.dgvData.ReadOnly = true;
+            this.dgvData.RowHeadersWidth = 51;
+            this.dgvData.RowTemplate.Height = 24;
+            this.dgvData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvData.Size = new System.Drawing.Size(988, 313);
+            this.dgvData.TabIndex = 0;
+            this.dgvData.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvData_CellClick);
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtFirstName.Location = new System.Drawing.Point(138, 84);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 22);
+            this.txtFirstName.TabIndex = 1;
+            // 
+            // txtMidName
+            // 
+            this.txtMidName.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtMidName.Location = new System.Drawing.Point(331, 84);
+            this.txtMidName.Name = "txtMidName";
+            this.txtMidName.Size = new System.Drawing.Size(100, 22);
+            this.txtMidName.TabIndex = 2;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtLastName.Location = new System.Drawing.Point(535, 84);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 22);
+            this.txtLastName.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(51, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "FirstName";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(251, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "MidName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(449, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "LastName";
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnInsert.Location = new System.Drawing.Point(641, 78);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 28);
+            this.btnInsert.TabIndex = 7;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDelete.Location = new System.Drawing.Point(722, 78);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 28);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnUpdate.Location = new System.Drawing.Point(803, 78);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 28);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSave.Location = new System.Drawing.Point(884, 78);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(79, 28);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1012, 475);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtMidName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.dgvData);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtMidName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ ConnectionPostgres/Form1.cs
@@ -0,0 +1,183 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConnectionPostgres
+{
+    public partial class Form1 : Form
+    {
+        string connstring = String.Format("Server = {0}; Port = {1};" +
+            "User Id = {2}; Password = {3}; Database = {4};",
+            "localhost", "5432", "postgres",
+            "yura12345", "Demo");
+
+        private NpgsqlConnection conn;
+        private string sql;
+        private NpgsqlCommand cmd;
+        private DataTable dt;
+        private int rowIndex = -1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new NpgsqlConnection(connstring);
+            Select();
+        }
+
+        private void Select()
+        {
+            try
+            {
+                conn.Open();
+                sql = @"select * from st_select()";
+                cmd = new NpgsqlCommand(sql, conn);
+                dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                conn.Close();
+                dgvData.DataSource = null;
+                dgvData.DataSource = dt;
+            }
+            catch (Exception ex) 
+            {
+                conn.Close();
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void dgvData_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex >= 0)
+            {
+                rowIndex = e.RowIndex;  
+                txtFirstName.Text = dgvData.Rows[e.RowIndex].Cells["firstname"].Value.ToString();
+                txtMidName.Text = dgvData.Rows[e.RowIndex].Cells["midname"].Value.ToString();
+                txtLastName.Text = dgvData.Rows[e.RowIndex].Cells["lastname"].Value.ToString();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (rowIndex < 0)
+            {
+                MessageBox.Show("Please choose student to delete");
+                return;
+            }
+            try
+            {
+                conn.Open();
+                sql = @"select * from st_delete(:_id)";
+                cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("_id", int.Parse(dgvData.Rows[rowIndex].Cells["id"].Value.ToString()));
+                if((int)cmd.ExecuteScalar() == 1)
+                {
+                    MessageBox.Show("Delete Student successfully");
+                    rowIndex = -1;
+                    Select();
+                }
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show("Deleted fail. Error: " + ex.Message);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+            if(rowIndex < 0)
+            {
+                MessageBox.Show("Please choose student to update");
+                return;
+            }
+            txtFirstName.Enabled = txtMidName.Enabled = txtLastName.Enabled = true;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            int result = 0;
+            if (rowIndex < 0)
+            {
+                try
+                {
+                    conn.Open();
+                    sql = @"select * from st_insert(:_firstname, :_midname, :_lastname)";
+                    cmd = new NpgsqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("_firstname", txtFirstName.Text);
+                    cmd.Parameters.AddWithValue("_midname", txtMidName.Text);
+                    cmd.Parameters.AddWithValue("_lastname", txtLastName.Text);
+                    result = (int)cmd.ExecuteScalar();
+                    conn.Close();
+                    if(result == 1)
+                    {
+                        MessageBox.Show("Inserted new student successfully");
+                        Select();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Inserted fail");
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    conn.Close();
+                    MessageBox.Show("Inserted fail. Error: " + ex.Message);
+                }
+            }
+            else
+            {
+                try
+                {
+                    conn.Open();
+                    sql = @"select * from st_update(:_id, :_firstname, :_midname, :_lastname)";
+                    cmd.Parameters.AddWithValue("_id", int.Parse(dgvData.Rows[rowIndex].Cells["id"].Value.ToString()));
+                    cmd.Parameters.AddWithValue("_firstname", txtFirstName.Text);
+                    cmd.Parameters.AddWithValue("_midname", txtMidName.Text);
+                    cmd.Parameters.AddWithValue("_lastname", txtLastName.Text);
+                    result = (int)cmd.ExecuteScalar();
+                    conn.Close();
+                    if(result == 1)
+                    {
+                        MessageBox.Show("Update successfully");
+                        Select();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Update fail");
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    conn.Close();
+                    MessageBox.Show("Update fail. Error: " + ex.Message);
+                }
+                
+            }
+            result = 0;
+            txtFirstName.Text = txtMidName.Text = txtLastName.Text = null;
+            txtFirstName.Enabled = txtMidName.Enabled = txtLastName.Enabled = false;
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+
+            rowIndex = -1;
+            txtFirstName.Enabled = txtMidName.Enabled = txtLastName.Enabled = true;
+            txtFirstName.Text = txtMidName.Text = txtLastName.Text = null;
+            txtFirstName.Select();
+        }
+    }
+}
